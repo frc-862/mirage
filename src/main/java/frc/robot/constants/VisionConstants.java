c8cf8dd963c6ef7dc2c523cb52da40cee41c8965
Zan e1 <zbeidas@icloud.com>
2026-01-09 22:25:15 -0500

    [#10] Finished a functional vision subsystem (no setters or getters yet)

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -1,7 +1,27 @@
 package frc.robot.constants;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Transform3d;
+
 public class VisionConstants {
-    public static enum CameraPosition {
-        FRONT_RIGHT, BACK_RIGHT, FRONT_LEFT, BACK_LEFT
+    // TODO: Update this to be the correect field layout for this season,
+    public static final AprilTagFieldLayout TAG_LAYOUT = AprilTagFieldLayout
+            .loadField(AprilTagFields.k2025ReefscapeWelded);
+
+
+    public static enum CameraInfo {
+
+        FRONT_RIGHT("frontRight", new Transform3d()), 
+        BACK_RIGHT("backRight", new Transform3d()), 
+        FRONT_LEFT("frontLeft", new Transform3d()),
+        BACK_LEFT("backLeft", new Transform3d());
+
+        public Transform3d offset;
+        public String name;
+        CameraInfo(String name, Transform3d offset) {
+            this.offset = offset;
+            this.name = name;
+        }
     }
 }
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.*;
-
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
+
+import org.photonvision.EstimatedRobotPose;
 
 import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.Utils;
@@ -15,11 +15,14 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
 import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Volts;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
@@ -271,26 +274,50 @@ public class Swerve extends TunerSwerveDrivetrain implements Subsystem {
         super.addVisionMeasurement(visionRobotPoseMeters, Utils.fpgaToCurrentTime(timestampSeconds));
     }
 
-    /**
-     * Adds a vision measurement to the Kalman Filter. This will correct the odometry pose estimate
-     * while still accounting for measurement noise.
-     * <p>
-     * Note that the vision measurement standard deviations passed into this method
-     * will continue to apply to future measurements until a subsequent call to
-     * {@link #setVisionMeasurementStdDevs(Matrix)} or this method.
-     *
-     * @param visionRobotPoseMeters The pose of the robot as measured by the vision camera.
-     * @param timestampSeconds The timestamp of the vision measurement in seconds.
-     * @param visionMeasurementStdDevs Standard deviations of the vision pose measurement
-     *     in the form [x, y, theta]ᵀ, with units in meters and radians.
-     */
-    @Override
-    public void addVisionMeasurement(
-        Pose2d visionRobotPoseMeters,
-        double timestampSeconds,
-        Matrix<N3, N1> visionMeasurementStdDevs
-    ) {
-        super.addVisionMeasurement(visionRobotPoseMeters, Utils.fpgaToCurrentTime(timestampSeconds), visionMeasurementStdDevs);
+    // /**
+    //  * Adds a vision measurement to the Kalman Filter. This will correct the odometry pose estimate
+    //  * while still accounting for measurement noise.
+    //  * <p>
+    //  * Note that the vision measurement standard deviations passed into this method
+    //  * will continue to apply to future measurements until a subsequent call to
+    //  * {@link #setVisionMeasurementStdDevs(Matrix)} or this method.
+    //  *
+    //  * @param visionRobotPoseMeters The pose of the robot as measured by the vision camera.
+    //  * @param timestampSeconds The timestamp of the vision measurement in seconds.
+    //  * @param visionMeasurementStdDevs Standard deviations of the vision pose measurement
+    //  *     in the form [x, y, theta]ᵀ, with units in meters and radians.
+    //  */
+    // @Override
+    // public void addVisionMeasurement(
+    //     Pose2d visionRobotPoseMeters,
+    //     double timestampSeconds,
+    //     Matrix<N3, N1> visionMeasurementStdDevs
+    // ) {
+    //     super.addVisionMeasurement(visionRobotPoseMeters, Utils.fpgaToCurrentTime(timestampSeconds), visionMeasurementStdDevs);
+    // }
+
+    public void addVisionMeasurement(EstimatedRobotPose pose, double distance) {
+        if (DriverStation.isDisabled()) {
+            addVisionMeasurement(pose.estimatedPose.toPose2d(), Utils.fpgaToCurrentTime(pose.timestampSeconds),
+                    VecBuilder.fill(0.1, 0.1, 0.1));
+        } else {
+            // addVisionMeasurement(pose.estimatedPose.toPose2d(),
+            // Utils.fpgaToCurrentTime(pose.timestampSeconds),
+            // VecBuilder.fill(VisionConstants.VISION_X_STDEV,
+            // VisionConstants.VISION_Y_STDEV, VisionConstants.VISION_THETA_STDEV));
+
+            // if(distance < 0.25) {
+            // addVisionMeasurement(pose.estimatedPose.toPose2d(),
+            // Utils.fpgaToCurrentTime(pose.timestampSeconds),
+            // VecBuilder.fill(0.01, 0.01, 0.01));
+            // } else {
+
+            // for ambiguity-based (or distance-based) std deviations
+            addVisionMeasurement(pose.estimatedPose.toPose2d(), Utils.fpgaToCurrentTime(pose.timestampSeconds),
+                    VecBuilder.fill(distance / 2, distance / 2, distance / 2));
+            // }
+
+        }
     }
 
     public Command driveCommand(DoubleSupplier xInput, DoubleSupplier yInput, DoubleSupplier rInput) {
