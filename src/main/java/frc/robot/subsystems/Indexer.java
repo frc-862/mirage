3fbb05d493d709c985f988c61d37953595d97fc2
TechKidJr <kidannyjunior10@gmail.com>
2026-01-13 19:17:51 -0500

    [#34] - Created Basic Indexer Subsystem
    Created Basic Indexer Subsystem and modified constants.

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -4,14 +4,39 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.DutyCycleOut;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.RobotMap;
+import frc.util.hardware.ThunderBird;
 
 public class Indexer extends SubsystemBase {
-  /** Creates a new Indexer. */
-  public Indexer() {}
+    ThunderBird indexerMotor;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    /** Creates a new Indexer. */
+    public Indexer() {
+        indexerMotor = new ThunderBird(RobotMap.INDEXER_MOTOR_ID, RobotMap.CAN_BUS_NAME,
+            RobotMap.INDEXER_MOTOR_INVERTED, RobotMap.INDEXER_MOTOR_STATOR_LIMIT, RobotMap.INDEXER_MOTOR_BRAKE_MODE);
+    }
+
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    /**
+     * Sets power to (-1 to 1) to the indexer motor.
+     * @param power
+     */
+    public void setPower(double power) {
+        indexerMotor.setControl(new DutyCycleOut(power));
+    }
+
+    /**
+     * Stops the indexer motor.
+     */
+    public void stop() {
+        indexerMotor.stopMotor();
+    }
 }
