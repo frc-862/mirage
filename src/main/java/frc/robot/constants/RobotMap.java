e80c07f7ba2b02f8ef18b80b81fa63a7fe5bf25e
The17thServant <bman135222@gmail.com>
2026-01-13 16:18:18 -0800

    [#33] - basic shooter / Created basic shooter subsystem

--- src/main/java/frc/robot/constants/RobotMap.java
+++ src/main/java/frc/robot/constants/RobotMap.java
@@ -3,7 +3,24 @@ package frc.robot.constants;
 import java.nio.file.Paths;
 
 public class RobotMap {
-    
+
     public static final String OASIS_IDENTIFIER = "/home/lvuser/Oasis"; // Differentiate between Oasis and Mirage
     public static final boolean IS_OASIS = Paths.get(OASIS_IDENTIFIER).toFile().exists();
+
+
+    public static final String CAN_BUS_NAME = "canbus"; //temp
+
+    public static final int SHOOTER_BOTTOM_MOTOR_ID = 3; //temp
+    public static final int SHOOTER_TOP_MOTOR_ID = 4; //temp
+
+    public static final boolean SHOOTER_BOTTOM_MOTOR_INVERTED = false; //temp
+    public static final boolean SHOOTER_TOP_MOTOR_INVERTED = false; //temp
+
+    public static final double SHOOTER_BOTTOM_MOTOR_STATOR_LIMIT = 120.0; //temp
+    public static final double SHOOTER_TOP_MOTOR_STATOR_LIMIT = 120.0; //temp
+
+    public static final boolean SHOOTER_BOTTOM_MOTOR_BRAKE = true; //temp
+    public static final boolean SHOOTER_TOP_MOTOR_BRAKE = true; //temp
+
 }
+
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,15 +4,47 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.DutyCycleOut;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.RobotMap;
+import frc.util.hardware.ThunderBird;
 
 public class Shooter extends SubsystemBase {
-  /** Creates a new Shooter. */
-  public Shooter() {}
-  
+    /**Creates new motors */
+    ThunderBird topMotor;
+    ThunderBird bottomMotor;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    /** Creates a new Shooter. */
+    public Shooter() {
+        //Sets new motors
+        topMotor = new ThunderBird(RobotMap.SHOOTER_TOP_MOTOR_ID, RobotMap.CAN_BUS_NAME,
+            RobotMap.SHOOTER_TOP_MOTOR_INVERTED, RobotMap.SHOOTER_TOP_MOTOR_STATOR_LIMIT, RobotMap.SHOOTER_TOP_MOTOR_BRAKE);
+        bottomMotor = new ThunderBird(RobotMap.SHOOTER_BOTTOM_MOTOR_ID, RobotMap.CAN_BUS_NAME,
+            RobotMap.SHOOTER_BOTTOM_MOTOR_INVERTED, RobotMap.SHOOTER_BOTTOM_MOTOR_STATOR_LIMIT, RobotMap.SHOOTER_BOTTOM_MOTOR_BRAKE);
+    }
+
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+
+    }
+    /**Set top motor power */
+    public void setTopPower(double power) {
+        topMotor.setControl(new DutyCycleOut(power));
+
+    }
+    /**Set top motor power */
+    public void setBottomPower(double power) {
+        bottomMotor.setControl(new DutyCycleOut(power));
+    }
+    /**Stoping the top Motor */
+    public void stopTopMotor() {
+        topMotor.stopMotor();
+    }
+    /**Stoping the bottom motor */
+    public void stopBottomMotor() {
+        bottomMotor.stopMotor();
+    }
 }
