e421454db5c02781df7260e9b66d7c3e58c86060
Karthik <krajendran275@gmail.com>
2026-01-13 21:16:42 -0500

    Finished PBAA and Constants
    Has not been tested in sim yet

--- src/main/java/frc/robot/commands/PoseBasedAutoAlign.java
+++ src/main/java/frc/robot/commands/PoseBasedAutoAlign.java
@@ -4,22 +4,61 @@
 
 package frc.robot.commands;
 
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.PoseConstants;
+import frc.robot.subsystems.Swerve;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class PoseBasedAutoAlign extends Command {
+
+  //creates drivertrain variable
+  private Swerve drivetrain;
+
+  private PIDController pidX;
+  private PIDController pidY;
+  private PIDController pidR;
+
+  //Creates a variable for DriveTolerance (amount of possible error)
+  private double DriveTolerance = PoseConstants.TELE_DRIVE_TOLERANCE;
+
+  //creates a variable for the PID controller for the X-axis (horizontally)
+
+  private Pose2d targetPose = new Pose2d();
   /** Creates a new PoseBasedAutoAlign. */
-  public PoseBasedAutoAlign() {
+  public PoseBasedAutoAlign(Swerve drivetrain) {
+    //sets drivetrain
+    this.drivetrain = drivetrain;
+
+    //sets the pid values to a pid controller
+    pidX = new PIDController(PoseConstants.AUTON_DRIVE_P, PoseConstants.AUTON_DRIVE_I, PoseConstants.AUTON_DRIVE_D);
+    pidY = new PIDController(PoseConstants.AUTON_DRIVE_P, PoseConstants.AUTON_DRIVE_I, PoseConstants.AUTON_DRIVE_D);
+    pidR = new PIDController(PoseConstants.AUTON_DRIVE_P, PoseConstants.AUTON_DRIVE_I, PoseConstants.AUTON_DRIVE_D);
+
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivetrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    //gives a tolerance to pidX and Y
+    pidX.setTolerance(DriveTolerance);
+    pidY.setTolerance(DriveTolerance);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    Pose2d currentPose = drivetrain.getPose();
+
+    drivetrain.setControl(new SwerveRequest.FieldCentric()
+      .withVelocityX(pidX.calculate(currentPose.getX(), targetPose.getX()))
+      .withVelocityY(pidY.calculate(currentPose.getY(), targetPose.getY())));
+  }
 
   // Called once the command ends or is interrupted.
   @Override
