9db48ebac0eedc232ceabfca0662391c75a3822c
mavanie <151957539+mavanie@users.noreply.github.com>
2026-01-15 20:16:46 -0500

    32 Basic Collector (#42)
    * Create collector subsystem
    
    * Add commands to collector
    
    * Instantiate collector. Bind Collector to Copilot A and B
    
    * [#32] - end of tuesday, pivot still wip
    
    * Add pivot to collector
    
    * Keep motor ID's in RobotMap.
    
    * Don't instantiate collector in RobotContainer.
    
    * Fix review findings
    
    ---------
    
    Co-authored-by: Maxim Vanier <maximvanier@gmail.com>
    Co-authored-by: Meh243 <dasandburg@gmail.com>

--- src/main/java/frc/robot/constants/RobotMap.java
+++ src/main/java/frc/robot/constants/RobotMap.java
@@ -5,17 +5,20 @@ import java.nio.file.Paths;
 import com.ctre.phoenix6.CANBus;
 
 public class RobotMap {
-
     public static final String OASIS_IDENTIFIER = "/home/lvuser/Oasis"; // Differentiate between Oasis and Mirage
     public static final boolean IS_OASIS = Paths.get(OASIS_IDENTIFIER).toFile().exists();
 
-    public static final CANBus CAN_BUS = new CANBus("canbus"); // Temp
+    public static final CANBus CAN_BUS = new CANBus("Canivore"); // Temp
 
-    // Indexer constants
+    // indexer motor
     public static final int INDEXER_MOTOR_ID = 1; // Temp
     public static final boolean INDEXER_MOTOR_INVERTED = false; // Temp
     public static final double INDEXER_MOTOR_STATOR_LIMIT = 120d; // Temp
     public static final boolean INDEXER_MOTOR_BRAKE_MODE = true; // Temp
 
+    // Collector / Pivot
+    public static final int COLLECTOR_MOTOR_ID = 2; // Temp
+    public static final int PIVOT_MOTOR_ID = 5; // Temp
+    public static final int PIVOT_ENCODER_ID = 35; // Temp
 }
 
