855913716a3397d6adf5f7f5184bafd42bdb9096
Kidanny R <kidannyjunior10@gmail.com>
2026-01-15 20:53:26 -0500

    [#45] Fixed intentation issues, moved most Hood constants to HoodConstants.java, made a new DutyCycleOut with dutyCycle

---
+++ src/main/java/frc/robot/constants/HoodConstants.java
@@ -0,0 +1,13 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.constants;
+
+/** Add your docs here. */
+public class HoodConstants {
+    public static final boolean HOOD_MOTOR_INVERTED = false; // Temp
+    public static final double HOOD_MOTOR_STATOR_LIMIT = 40d; // Temp
+    public static final boolean HOOD_MOTOR_BRAKE_MODE = false; // Temp
+
+}
--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -11,31 +11,34 @@ import frc.util.hardware.ThunderBird;
 import frc.robot.constants.RobotMap;
 
 public class Hood extends SubsystemBase {
-  private ThunderBird hoodMotor;
-  /** Creates a new Hood. */
-  public Hood() {
-    hoodMotor = new ThunderBird(RobotMap.HOOD_MOTOR_ID, RobotMap.CAN_BUS,
-       RobotMap.HOOD_MOTOR_INVERTED, RobotMap.HOOD_MOTOR_STATOR_LIMIT,
-        RobotMap.HOOD_MOTOR_BRAKE_MODE);
-  }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    public final DutyCycleOut dutyCycle = new DutyCycleOut(0.0);
 
-  /**
-   * Sets power to (-1 to 1) to the hood motor.
-   * @param power
-   */
-  public void setPower(double power) {
-    hoodMotor.setControl(new DutyCycleOut(power));
-  }
+    private ThunderBird hoodMotor;
+    /** Creates a new Hood. */
+    public Hood() {
+        hoodMotor = new ThunderBird(RobotMap.HOOD_MOTOR_ID, RobotMap.CAN_BUS,
+            RobotMap.HOOD_MOTOR_INVERTED, RobotMap.HOOD_MOTOR_STATOR_LIMIT,
+            RobotMap.HOOD_MOTOR_BRAKE_MODE);
+    }
 
-  /**
-   * Stops the hood motor.
-   */
-  public void stop() {
-    hoodMotor.stopMotor();
-  }
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    /**
+     * Sets power to (-1 to 1) to the hood motor.
+     * @param power
+     */
+    public void setPower(double power) {
+        hoodMotor.setControl(dutyCycle.withOutput(power));
+    }
+
+    /**
+     * Stops the hood motor.
+     */
+    public void stop() {
+        hoodMotor.stopMotor();
+    }
 }
