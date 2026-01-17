40fa50f3d8183a573dfe74ba223a349874e154cd
Vilok1 <147673254+Vilok1@users.noreply.github.com>
2026-01-17 08:10:37 -0500

    update wpilib (#58)
    * update wpilib, use new tag layout, update vendordeps
    
    * update comment

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -9,8 +9,8 @@ import edu.wpi.first.math.geometry.Transform3d;
 public class VisionConstants {
     public enum CameraInfo {
 
-        FRONT_RIGHT("frontRight", new Transform3d()), 
-        BACK_RIGHT("backRight", new Transform3d()), 
+        FRONT_RIGHT("frontRight", new Transform3d()),
+        BACK_RIGHT("backRight", new Transform3d()),
         FRONT_LEFT("frontLeft", new Transform3d()),
         BACK_LEFT("backLeft", new Transform3d());
 
@@ -25,9 +25,8 @@ public class VisionConstants {
 
     public static final List<Short> TAG_IGNORE_LIST = List.of();
 
-    // TODO: Update this to be the correect field layout for this season,
     public static final AprilTagFieldLayout TAG_LAYOUT = AprilTagFieldLayout
-            .loadField(AprilTagFields.kDefaultField);
+            .loadField(AprilTagFields.k2026RebuiltWelded); //FIM Uses welded fields
 
     public static final double POSE_AMBIGUITY_TOLERANCE = 0.2;
     public static final double TAG_DISTANCE_TOLERANCE = 4;
