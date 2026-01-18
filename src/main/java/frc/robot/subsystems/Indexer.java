f76ce1e5c758467a1ddfca872896a234c9b02b70
Vilok1 <147673254+Vilok1@users.noreply.github.com>
2026-01-17 19:19:04 -0500

    start

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -6,24 +6,41 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.controls.DutyCycleOut;
 
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.LinearSystemSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 import frc.robot.constants.IndexerConstants;
 import frc.robot.constants.RobotMap;
 import frc.util.hardware.ThunderBird;
 
 public class Indexer extends SubsystemBase {
-    ThunderBird indexerMotor;
+    private final ThunderBird indexerMotor;
+    private final DutyCycleOut dutyCycle;
+
+    private final LinearSystemSim<N2, N1, N2> indexerSim;
 
     /** Creates a new Indexer. */
     public Indexer() {
         indexerMotor = new ThunderBird(RobotMap.INDEXER_MOTOR_ID, RobotMap.CAN_BUS,
             IndexerConstants.INDEXER_MOTOR_INVERTED, IndexerConstants.INDEXER_MOTOR_STATOR_LIMIT, IndexerConstants.INDEXER_MOTOR_BRAKE_MODE);
+
+        dutyCycle = new DutyCycleOut(0);
+
+        if (Robot.isSimulation()){
+            indexerSim = new LinearSystemSim<N2, N1, N2>(LinearSystemId.createDCMotorSystem(0.3, 0.3));
+        }
     }
 
 
     @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
+    public void simulationPeriodic() {
+        indexerMotor.getSimState().setSupplyVoltage(RobotController.getBatteryVoltage());
+        indexerSim.setInput(indexerMotor.getSimState().getMotorVoltage());
+        indexerSim.update(0.02);
     }
 
     /**
@@ -31,7 +48,7 @@ public class Indexer extends SubsystemBase {
      * @param power
      */
     public void setPower(double power) {
-        indexerMotor.setControl(new DutyCycleOut(power));
+        indexerMotor.setControl(dutyCycle.withOutput(power));
     }
 
     /**
