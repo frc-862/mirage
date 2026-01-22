97208ccb1e215ded84333870ef2d9adccba90b27
mavanie <151957539+mavanie@users.noreply.github.com>
2026-01-21 20:37:46 -0500

    89 rename subsystems (#90)
    * Change Indexer to Spindexer
    
    * Change Shooter to Flywheel
    
    * Change TurretTower to Kicker
    
    * Change Kicker to Transfer
    
    ---------
    
    Co-authored-by: Maxim Vanier <maximvanier@gmail.com>

--- src/main/java/frc/robot/subsystems/Spindexer.java
+++ src/main/java/frc/robot/subsystems/Spindexer.java
@@ -18,29 +18,29 @@ import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.LinearSystemSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
-import frc.robot.constants.IndexerConstants;
+import frc.robot.constants.SpindexerConstants;
 import frc.robot.constants.RobotMap;
 import frc.util.hardware.ThunderBird;
 import frc.util.shuffleboard.LightningShuffleboard;
 
-public class Indexer extends SubsystemBase {
-    private final ThunderBird indexerMotor;
+public class Spindexer extends SubsystemBase {
+    private final ThunderBird spindexerMotor;
     private final DutyCycleOut dutyCycle;
 
-    private LinearSystemSim<N1, N1, N1> indexerSim;
+    private LinearSystemSim<N1, N1, N1> spindexerSim;
     private TalonFXSimState motorSim;
 
-    /** Creates a new Indexer. */
-    public Indexer() {
-        indexerMotor = new ThunderBird(RobotMap.INDEXER_MOTOR_ID, RobotMap.CAN_BUS,
-            IndexerConstants.INDEXER_MOTOR_INVERTED, IndexerConstants.INDEXER_MOTOR_STATOR_LIMIT, IndexerConstants.INDEXER_MOTOR_BRAKE_MODE);
+    /** Creates a new Spindexer. */
+    public Spindexer() {
+        spindexerMotor = new ThunderBird(RobotMap.SPINDEXER_MOTOR_ID, RobotMap.CAN_BUS,
+            SpindexerConstants.SPINDEXER_MOTOR_INVERTED, SpindexerConstants.SPINDEXER_MOTOR_STATOR_LIMIT, SpindexerConstants.SPINDEXER_MOTOR_BRAKE_MODE);
 
         dutyCycle = new DutyCycleOut(0);
 
         if (Robot.isSimulation()) {
-            indexerSim = new LinearSystemSim<N1, N1, N1>(LinearSystemId.identifyVelocitySystem(IndexerConstants.SIM_kV,
-                IndexerConstants.SIM_kA));
-            motorSim = indexerMotor.getSimState();
+            spindexerSim = new LinearSystemSim<N1, N1, N1>(LinearSystemId.identifyVelocitySystem(SpindexerConstants.SIM_kV,
+                SpindexerConstants.SIM_kA));
+            motorSim = spindexerMotor.getSimState();
             motorSim.setMotorType(MotorType.KrakenX44);
         }
     }
@@ -49,35 +49,35 @@ public class Indexer extends SubsystemBase {
     public void simulationPeriodic() {
         motorSim.setSupplyVoltage(RobotController.getBatteryVoltage());
 
-        indexerSim.setInput(motorSim.getMotorVoltageMeasure().in(Volts));
-        indexerSim.update(Robot.kDefaultPeriod);
+        spindexerSim.setInput(motorSim.getMotorVoltageMeasure().in(Volts));
+        spindexerSim.update(Robot.kDefaultPeriod);
 
-        motorSim.setRotorVelocity(indexerSim.getOutput(0));
+        motorSim.setRotorVelocity(spindexerSim.getOutput(0));
 
-        LightningShuffleboard.setDouble("Indexer", "Velocity", getVelocity().in(RotationsPerSecond));
+        LightningShuffleboard.setDouble("Spindexer", "Velocity", getVelocity().in(RotationsPerSecond));
     }
 
     /**
-     * Sets power to (-1 to 1) to the indexer motor.
+     * Sets power to (-1 to 1) to the spindexer motor.
      * @param power
      */
     public void setPower(double power) {
-        indexerMotor.setControl(dutyCycle.withOutput(power));
+        spindexerMotor.setControl(dutyCycle.withOutput(power));
     }
 
     /**
-     * Gets the current velocity of the indexer motor.
+     * Gets the current velocity of the spindexer motor.
      *
-     * @return the indexer motor velocity as an {@link AngularVelocity}
+     * @return the spindexer motor velocity as an {@link AngularVelocity}
      */
     public AngularVelocity getVelocity() {
-        return indexerMotor.getVelocity().getValue();
+        return spindexerMotor.getVelocity().getValue();
     }
 
     /**
-     * Stops the indexer motor.
+     * Stops the spindexer motor.
      */
     public void stop() {
-        indexerMotor.stopMotor();
+        spindexerMotor.stopMotor();
     }
 }
