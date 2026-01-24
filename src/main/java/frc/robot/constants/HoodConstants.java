ab5ef1b3d4985b1554362343a1c2c88b8f2324ea
kananya19-beep <k.ananya19@gmail.com>
2026-01-24 15:24:38 -0500

    [#111] Changed all temps to match formatting in comments. (#115)
    * [#111] Changed all temps to match formatting in comments.
    
    * [#111] Fixed errors caused by merge conflicts
    
    * [#111] Changed all "temps" from RobotMap and Constants classes to lower case.
    
    * [#111] removed temp comments
    
    ---------
    
    Co-authored-by: EduardoBarreto3006 <eduardorsbarreto@gmail.com>

--- src/main/java/frc/robot/constants/HoodConstants.java
+++ src/main/java/frc/robot/constants/HoodConstants.java
@@ -10,22 +10,22 @@ import edu.wpi.first.units.measure.Angle;
 
 /** Constants for Hood */
 public class HoodConstants {
-    public static final boolean INVERTED = false; // Temp
-    public static final double STATOR_LIMIT = 40d; // Temp
-    public static final boolean BRAKE = false; // Temp
+    public static final boolean INVERTED = false; // temp
+    public static final double STATOR_LIMIT = 40d; // temp
+    public static final boolean BRAKE = false; // temp
     public static final double HOOD_MOTOR_POWER = 1d;
 
     // https://v6.docs.ctr-electronics.com/en/latest/docs/api-reference/device-specific/talonfx/motion-magic.html
-    public static final double kS = 0.0; // TEMP
-    public static final double kV = 0.0; // TEMP
-    public static final double kA = 0.0; // TEMP
-    public static final double kP = 0.0; // TEMP
-    public static final double kI = 0.0; // TEMP
-    public static final double kD = 0.0; // TEMP
+    public static final double kS = 0.0; // temp
+    public static final double kV = 0.0; // temp
+    public static final double kA = 0.0; // temp
+    public static final double kP = 0.0; // temp
+    public static final double kI = 0.0; // temp
+    public static final double kD = 0.0; // temp
     // Motion Magic parameters (https://v6.docs.ctr-electronics.com/en/latest/docs/api-reference/device-specific/talonfx/motion-magic.html)
-    public static final double CRUISE_VELOCITY = 80.0; // TEMP
-    public static final double ACCELERATION = 160.0; // TEMP
-    public static final double JERK = 1600.0; // TEMP
+    public static final double CRUISE_VELOCITY = 80.0; // temp
+    public static final double ACCELERATION = 160.0; // temp
+    public static final double JERK = 1600.0; // temp
     // Angle limits
     public static final Angle MIN_ANGLE = Degrees.of(0); // temp
     public static final Angle MAX_ANGLE = Degrees.of(90); // temp
