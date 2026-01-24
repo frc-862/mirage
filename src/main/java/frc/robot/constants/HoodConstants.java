de01c3f275cbe9dadf7907c645ee78638a0ed528
TechKidJr <kidannyjunior10@gmail.com>
2026-01-24 08:51:49 -0500

    [#54] Created MotionMagic and TalonFXConfig.

--- src/main/java/frc/robot/constants/HoodConstants.java
+++ src/main/java/frc/robot/constants/HoodConstants.java
@@ -9,5 +9,15 @@ public class HoodConstants {
     public static final boolean HOOD_MOTOR_INVERTED = false; // Temp
     public static final double HOOD_MOTOR_STATOR_LIMIT = 40d; // Temp
     public static final boolean HOOD_MOTOR_BRAKE_MODE = false; // Temp
-
+    // https://v6.docs.ctr-electronics.com/en/latest/docs/api-reference/device-specific/talonfx/motion-magic.html
+    public static final double kS = 0.0; // TEMP
+    public static final double kV = 0.0; // TEMP
+    public static final double kA = 0.0; // TEMP
+    public static final double kP = 0.0; // TEMP
+    public static final double kI = 0.0; // TEMP
+    public static final double kD = 0.0; // TEMP
+    // Motion Magic parameters (https://v6.docs.ctr-electronics.com/en/latest/docs/api-reference/device-specific/talonfx/motion-magic.html)
+    public static final double CruiseVelocity = 80.0; // TEMP
+    public static final double Acceleration = 160.0; // TEMP
+    public static final double Jerk = 1600.0; // TEMP
 }
