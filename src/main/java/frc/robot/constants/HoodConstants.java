a564c7370aa88d1a66f72fa3c50aea3fc409cf8a
SripaadaK111 <shridatta700@gmail.com>
2026-01-24 09:58:58 -0500

    [#101] Created Hood Constants and Added Sim

--- src/main/java/frc/robot/constants/HoodConstants.java
+++ src/main/java/frc/robot/constants/HoodConstants.java
@@ -4,10 +4,37 @@
 
 package frc.robot.constants;
 
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.MomentOfInertia;
+
+import static edu.wpi.first.units.Units.Degree;
+import static edu.wpi.first.units.Units.Meter;
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
+
+
 /** Constants for Hood */
 public class HoodConstants {
     public static final boolean HOOD_MOTOR_INVERTED = false; // Temp
     public static final double HOOD_MOTOR_STATOR_LIMIT = 40d; // Temp
     public static final boolean HOOD_MOTOR_BRAKE_MODE = false; // Temp
+
+    public static final Angle MIN_ANGLE = Degree.of(0); // Temp
+    public static final Angle MAX_ANGLE = Degree.of(90); // Temp
+
+    public static final double MOTOR_KP = 0; // Temp
+    public static final double MOTOR_KI = 0; // Temp
+    public static final double MOTOR_KD = 0; // Temp
+    public static final double MOTOR_KF = 0; // Temp
+    public static final double MOTOR_KS = 0; // Temp
+    public static final double MOTOR_KV = 0; // Temp
+    public static final double MOTOR_KA = 0; // Temp
+    public static final double MOTOR_KG = 0; // Temp
+
+    public static final double GEARING_RATIO = 75; // Temp
+
+    public static final MomentOfInertia MOI = KilogramSquareMeters.of(0.086); // Temp
+    public static final Distance LENGTH = Meter.of(0.50); // Temp
 
 }
--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -4,11 +4,26 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.controls.DutyCycleOut;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
 
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.util.hardware.ThunderBird;
+import frc.util.shuffleboard.LightningShuffleboard;
 import frc.robot.constants.RobotMap;
+import frc.robot.constants.TurretConstants;
+import frc.robot.Robot;
 import frc.robot.constants.HoodConstants;
 
 public class Hood extends SubsystemBase {
@@ -16,11 +31,27 @@ public class Hood extends SubsystemBase {
     public final DutyCycleOut dutyCycle = new DutyCycleOut(0.0);
 
     private ThunderBird hoodMotor;
+    private SingleJointedArmSim hoodSim;
+    private TalonFXSimState motorSim;
+    private DCMotor gearbox;
+
     /** Creates a new Hood. */
     public Hood() {
         hoodMotor = new ThunderBird(RobotMap.HOOD_MOTOR_ID, RobotMap.CAN_BUS,
             HoodConstants.HOOD_MOTOR_INVERTED, HoodConstants.HOOD_MOTOR_STATOR_LIMIT,
             HoodConstants.HOOD_MOTOR_BRAKE_MODE);
+
+        if (Robot.isSimulation()) {
+            gearbox = DCMotor.getKrakenX44Foc(1);
+            hoodSim = new SingleJointedArmSim(gearbox, HoodConstants.GEARING_RATIO,
+                HoodConstants.MOI.magnitude(), HoodConstants.LENGTH.in(Meters),
+                HoodConstants.MIN_ANGLE.in(Radians), HoodConstants.MAX_ANGLE.in(Radians),
+                false, HoodConstants.MIN_ANGLE.in(Radians), 0d, 1d);
+
+
+            motorSim = new TalonFXSimState(hoodMotor);
+            motorSim.setRawRotorPosition(TurretConstants.MIN_ANGLE.in(Rotations));
+        }
     }
 
     @Override
@@ -36,6 +67,24 @@ public class Hood extends SubsystemBase {
         hoodMotor.setControl(dutyCycle.withOutput(power));
     }
 
+
+    @Override
+    public void simulationPeriodic() {
+        double batteryVoltage = RobotController.getBatteryVoltage();
+        motorSim.setSupplyVoltage(batteryVoltage);
+
+        hoodSim.setInputVoltage(motorSim.getMotorVoltage());
+        hoodSim.update(Robot.kDefaultPeriod);
+
+        Angle simAngle = Rotations.of(hoodSim.getAngleRads());
+        AngularVelocity simVeloc = RadiansPerSecond.of(hoodSim.getVelocityRadPerSec());
+
+        motorSim.setRawRotorPosition(simAngle);
+        motorSim.setRotorVelocity(simVeloc);
+
+        LightningShuffleboard.setDouble("Hood", "Sim Angle", simAngle.in(Degrees));
+
+    }
     /**
      * Stops the hood motor.
      */
