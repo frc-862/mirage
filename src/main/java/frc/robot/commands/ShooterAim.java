18e6decd38448b6962cdb6f9a43e4c380814bb8e
Maxim Vanier <maximvanier@gmail.com>
2026-01-24 10:21:40 -0500

    Add hood to ShooterAim

--- src/main/java/frc/robot/commands/ShooterAim.java
+++ src/main/java/frc/robot/commands/ShooterAim.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import java.util.Map;
@@ -11,29 +12,40 @@ import java.util.Map;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Flywheel;
+import frc.robot.subsystems.Hood;
 import frc.robot.subsystems.Swerve;
 
 public class ShooterAim extends Command {
 
     private final Flywheel shooter;
     private final Swerve swerve;
+    private final Hood hood;
     private final Translation2d target;
 
-    // Input is in meters, output is in rotations per second
+    // Input is distance to target in meters, output is shooter speed in rotations per second
     private final InterpolatingDoubleTreeMap VELOCITY_MAP = InterpolatingDoubleTreeMap.ofEntries(
         Map.entry(2d, 20d),
         Map.entry(4d, 40d),
         Map.entry(6d, 60d)
     );
 
-    public ShooterAim(Flywheel shooter, Swerve swerve, Translation2d target) {
+    // Input is distance to target in meters, output is hood agle in degrees
+    private final InterpolatingDoubleTreeMap HOOD_MAP = InterpolatingDoubleTreeMap.ofEntries(
+        Map.entry(2d, 10d),
+        Map.entry(4d, 20d),
+        Map.entry(6d, 30d)
+    );
+
+    public ShooterAim(Flywheel shooter, Swerve swerve, Hood hood, Translation2d target) {
         this.shooter = shooter;
         this.swerve = swerve;
+        this.hood = hood;
         this.target = target;
-        addRequirements(shooter);
+        addRequirements(shooter, hood);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
@@ -43,7 +55,9 @@ public class ShooterAim extends Command {
         double distanceMeters = robotPose.getTranslation().getDistance(target);
 
         AngularVelocity shooterVelocity = RotationsPerSecond.of(VELOCITY_MAP.get(distanceMeters));
+        Angle hoodAngle = Degrees.of(HOOD_MAP.get(distanceMeters));
 
+        hood.setPosition(hoodAngle);
         shooter.setVelocity(shooterVelocity);
     }
 
