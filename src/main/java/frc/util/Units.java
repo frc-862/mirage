6b3b93d6c8b46b17423f8ae077cccd16ae41b85a
Vilok1 <147673254+Vilok1@users.noreply.github.com>
2026-01-29 11:56:33 -0500

    [#147] add modulus

--- src/main/java/frc/util/Units.java
+++ src/main/java/frc/util/Units.java
@@ -23,4 +23,31 @@ public class Units {
             return value;
         }
     }
+
+
+    /**
+     * Wraps a {@code Measure<U>} between a minimum and maximum.
+     *
+     * @param value
+     * @param min the lower bound (exclusive).
+     * @param max the upper bound (inclusive).
+     * @param <U> The unit type.
+     * @param <M> The measure type.
+     * @return A {@code Measure<U>} between {@code min} and {@code max}.
+     */
+    @SuppressWarnings("unchecked")
+    public static <U extends Unit, M extends Measure<U>> M inputModulus(M value, M min, M max) {
+        Measure<U> modulus = max.minus(min);
+
+        // Wrap input if it's above the maximum input
+        int numMax = (int) value.minus(min).div(modulus).magnitude();
+        value.minus(modulus.times(numMax));
+        value = (M) value.minus(modulus.times(numMax));
+
+        // Wrap input if it's below the minimum input
+        int numMin = (int) value.minus(max).div(modulus).magnitude();
+        value = (M) value.minus(modulus.times(numMin));
+
+        return value;
+    }
 }
