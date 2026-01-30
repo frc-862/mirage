fcf30199b3cbad4cf2633dbe1fdbe7c9bd9d7c2a
David Sandburg <120067923+Meh243@users.noreply.github.com>
2026-01-29 21:15:28 -0500

    93 code review week 3 (#138)
    * [#93] - fixed organization issues
    
    * [#93] - extra organization
    
    * [#93] - changed on target method
    
    * [#93] - fixed errors caused by merge conflicts

--- src/main/java/frc/robot/constants/PoseConstants.java
+++ src/main/java/frc/robot/constants/PoseConstants.java
@@ -6,7 +6,7 @@ public class PoseConstants {
         public static final double DRIVE_I = 0;
         public static final double DRIVE_D = 0.08;
         public static final double DRIVE_TOLERANCE = 0.025;
-        public static final double DRIVE_KS = 0;//0.08;
+        public static final double DRIVE_KS = 0;
 
         public static final double ROT_P = 0.03;
         public static final double ROT_I = 0;
