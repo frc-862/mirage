ccb207635964fa4081f4af6b102312187f86167c
EduardoBarreto3006 <eduardorsbarreto@gmail.com>
2026-01-29 21:17:19 -0500

    [#106] Run indexer as part of canned shot command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -118,7 +118,7 @@ public class RobotContainer {
             // test subsytems that aren't on the robot yet in here!
             new Trigger(driver::getAButton).whileTrue(new RunCommand(() -> hood.setPosition(HoodConstants.MAX_ANGLE), hood));
 
-            new Trigger(driver::getBButton).whileTrue(runCannedShot(CannedShot.HUB, shooter, hood, turret, drivetrain, leds));
+            new Trigger(driver::getBButton).whileTrue(runCannedShot(CannedShot.HUB, shooter, hood, turret, indexer, drivetrain, leds));
         }
     }
 
