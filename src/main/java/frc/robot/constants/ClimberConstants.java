7ce970426ed44b7239817a5fbb51d42377dc4c24
swordpartee <nabaile586@gmail.com>
2026-01-31 08:34:56 -0500

    updated constants

--- src/main/java/frc/robot/constants/ClimberConstants.java
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -10,4 +10,14 @@ public class ClimberConstants {
     public static final double CLIMBER_MOTOR_STATOR_LIMIT = 40d; // Temp
     public static final boolean CLIMBER_MOTOR_BRAKE_MODE = false; // Temp
 
+    public static final double CLIMB_KP = 0.0; // Temp
+    public static final double CLIMB_KI = 0.0; // Temp
+    public static final double CLIMB_KD = 0.0; // Temp
+    public static final double CLIMB_KS = 0.0; // Temp
+    public static final double CLIMB_KV = 0.0; // Temp
+    public static final double CLIMB_KA = 0.0; // Temp
+    public static final double CLIMB_KG = 0.0; // Temp
+
+    public static final double CLIMB_TOLERANCE = 0.5;
+
 }
