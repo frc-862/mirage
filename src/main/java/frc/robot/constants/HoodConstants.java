ea4f2bd5e73e61dff35835fb2f422f58d417b633
Maxim Vanier <maximvanier@gmail.com>
2026-01-31 10:27:48 -0500

    Change hood PID constants and gear ratio

--- src/main/java/frc/robot/constants/HoodConstants.java
+++ src/main/java/frc/robot/constants/HoodConstants.java
@@ -24,9 +24,9 @@ public class HoodConstants {
     public static final Angle MIN_ANGLE = Degree.of(-90); // Temp
     public static final Angle MAX_ANGLE = Degree.of(90); // Temp
 
-    public static final MomentOfInertia MOI = KilogramSquareMeters.of(0.086); // Temp
+    public static final MomentOfInertia MOI = KilogramSquareMeters.of(0.1); // Temp
     public static final Distance LENGTH = Meter.of(0.18); // Temp
-    public static final double GEARING_RATIO = 1d; // temp
+    public static final double GEARING_RATIO = 50d/22d*165d/15d; // temp
 
     // Input is distance to target in meters, output is hood angle in degrees
     public static final InterpolatingDoubleTreeMap HOOD_MAP = InterpolatingDoubleTreeMap.ofEntries(
@@ -34,18 +34,12 @@ public class HoodConstants {
         Map.entry(4d, 20d),
         Map.entry(6d, 30d));
 
-    // https://v6.docs.ctr-electronics.com/en/latest/docs/api-reference/device-specific/talonfx/motion-magic.html
-    public static final double kS = 1d; // temp
-    public static final double kV = 0.18; // temp
+    public static final double kS = 0.25d; // temp
+    public static final double kV = 0; // temp
     public static final double kA = 0.01; // temp
-    public static final double kP = 6.5; // temp
+    public static final double kP = 2; // temp
     public static final double kI = 0.0; // temp
     public static final double kD = 0.0; // temp
-    
-    // Motion Magic parameters (https://v6.docs.ctr-electronics.com/en/latest/docs/api-reference/device-specific/talonfx/motion-magic.html)
-    public static final double CRUISE_VELOCITY = 80.0; // temp
-    public static final double ACCELERATION = 160.0; // temp
-    public static final double JERK = 1600.0; // temp
 
     public static final Angle POSITION_TOLERANCE = Degree.of(0.41); // temp
 
