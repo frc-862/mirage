35b442d752f2ead262b39948b53180ed22476f14
Meh243 <dasandburg@gmail.com>
2026-02-05 20:49:05 -0500

    ran sysid

--- src/main/java/frc/robot/constants/MirageTunerConstants.java
+++ src/main/java/frc/robot/constants/MirageTunerConstants.java
@@ -30,8 +30,8 @@ public class MirageTunerConstants {
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(0.1).withKI(0).withKD(0)
-        .withKS(0).withKV(0.124);
+        .withKP(0.13602).withKI(0).withKD(0)
+        .withKS(0.21549).withKV(0.1208).withKA(0.0057772); 
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
