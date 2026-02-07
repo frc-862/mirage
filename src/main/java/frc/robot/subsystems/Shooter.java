46589825cb97f042989508f732beb7ad87d58034
GriffinPotot <pickle124w@gmail.com>
2026-02-07 13:22:45 -0500

    did coast command (#223)
    * did coast command
    
    * rcreate coast command
    
    * Change coast to use power so it doesn't use energy to decelerate
    
    * Remove unused import
    
    ---------
    
    Co-authored-by: Maxim Vanier <151957539+mavanie@users.noreply.github.com>
    Co-authored-by: Maxim Vanier <maximvanier@gmail.com>

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -32,6 +32,7 @@ import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
@@ -45,6 +46,7 @@ public class Shooter extends SubsystemBase {
         public static final boolean INVERTED = false; // temp
         public static final double STATOR_LIMIT = 120.0; // temp
         public static final boolean BRAKE = false; // temp
+        public static final double COAST_DC = 0.05; // Shooter power when coasting
 
         public static final double kP = 0.25d;
         public static final double kI = 0d;
@@ -195,9 +197,17 @@ public class Shooter extends SubsystemBase {
     /**
      * velocity control command for shooter
      * @param velocitySupplier
-        * @return the command for running the shooter
-        */
+     * @return the command for running the shooter
+     */
     public Command shootCommand(Supplier<AngularVelocity> velocitySupplier) {
         return new StartEndCommand(() -> setVelocity(velocitySupplier.get()), () -> {}, this).until(this::isOnTarget);
+    }
+
+    /**
+     * Sets shooter motor into an idle power
+     * @return the command for running the shooter at coast power
+     */
+    public Command coast() {
+        return new InstantCommand(() -> setPower(ShooterConstants.COAST_DC), this);
     }
 }
