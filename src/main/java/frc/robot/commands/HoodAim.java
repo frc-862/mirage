ead07af836c4b60694ad7988ad1f3b8714f08d6d
Advaith <advaithgirish12@gmail.com>
2026-02-14 15:52:50 -0500

    [#257] Created a target Angle that uses the Interpolation map.
    This target angle then is used to set position for hood.

--- src/main/java/frc/robot/commands/HoodAim.java
+++ src/main/java/frc/robot/commands/HoodAim.java
@@ -5,7 +5,11 @@
 package frc.robot.commands;
 
 import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
 import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Radians;
+
+import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -14,68 +18,61 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Hood;
+import frc.robot.subsystems.Hood.HoodConstants;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Shooter.ShooterConstants;
 import frc.robot.subsystems.Swerve;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class HoodAim extends Command {
-  /** Creates a new HoodAim. */
-  private Shooter shooter;
-  private Hood hood;
-  private Translation2d target;
-  private Distance distanceToTarget;
-  
-  /**
-   * @param drivetrain
-   * @param hood
-   * @param targetAngle 
-   * @param distanceToTargetMeters
-   */
-  public HoodAim(Shooter shooter, Hood hood, Translation2d target) {
-    this.shooter = shooter;
-    this.hood = hood;
-    this.target = target;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(hood);
-  }
+    /** Creates a new HoodAim. */
+    private Shooter shooter;
+    private Hood hood;
+    private Translation2d target;
+    private double distance; 
+    private double distanceToTarget;
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+    /**
+     * @param shooter Uses shooter to get the translation of where the shooter is on the field.
+     * @param hood Hood Subsystem.
+     * @param target Translation 2d for the target.
+     * @param distanceToTargetMeters Distance between the hood and the target position to aim at.
+     */
+    public HoodAim(Shooter shooter, Hood hood, Translation2d target, double distance) {
+        this.shooter = shooter;
+        this.hood = hood;
+        this.target = target;
+        this.distance = distance;
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    //Translation2d shooterTranslation = shooter.getShooterTranslation();
+        addRequirements(hood);
+    }
 
-    //distanceToTarget = Meters.of(robotPose.getTranslation().getDistance(target));
-    
+    @Override
+    public void initialize() {
+    }
 
+    @Override
+    public void execute() {
+        //Translation2d shooterTranslation = shooter.getShooterTranslation();
 
+        //distanceToTarget = Meters.of(shooterTranslation.getTranslation().getDistance(target));
 
+        Angle targetAngle = Radians.of(HoodConstants.HOOD_MAP.get(distance));
 
-    
+        hood.setPosition(targetAngle);
+        
 
     };
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    //hood.stop();
-  }
+    @Override
+    public void end(boolean interrupted) {
+        // hood.stop();
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 
-  private Angle OptimizeHoodAngle(Angle desired){
-    double targetDegrees = desired.in(Degrees);
-    double currentAngle = hood.getAngle().in(Degrees);
-    
-    double error = targetDegrees - currentAngle;
 
-    
-  }
 }
