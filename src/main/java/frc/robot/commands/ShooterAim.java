2b4f994d2e7ad7ea16b393fe218d9f2acc12a148
Advaith <advaithgirish12@gmail.com>
2026-02-18 20:26:20 -0500

    [#281] Removed hood set angle, unrequire hood, and turret, used the getShooterTranslation method.

--- src/main/java/frc/robot/commands/ShooterAim.java
+++ src/main/java/frc/robot/commands/ShooterAim.java
@@ -24,17 +24,11 @@ import frc.robot.subsystems.Turret;
 
 public class ShooterAim extends Command {
     private final Shooter shooter;
-    private final Swerve swerve;
+    private final Swerve drivetrain;
     private final Hood hood;
     private final Turret turret;
     private final Indexer indexer;
 
-    private Translation2d target;
-
-    private Pose2d pose = new Pose2d(0, 0, Rotation2d.fromDegrees(0)); //Temp
-    private Rectangle2d redAllianceZone = new Rectangle2d(pose, 0, 0); //Temp
-    private Rectangle2d neutralZone = new Rectangle2d(pose, 1, 1); //Temp
-    private Rectangle2d blueallianceZone = new Rectangle2d(pose, 2, 2); //Temp
 
     /**
      * Point the shooter and angle the hood so the fuel shot reaches the hub
@@ -44,49 +38,31 @@ public class ShooterAim extends Command {
      * @param turret the turret subsystem
      * @param indexer the indexer subs
      */
-    public ShooterAim(Shooter shooter, Swerve swerve, Hood hood, Turret turret, Indexer indexer){
+    public ShooterAim(Shooter shooter, Swerve drivetrain, Hood hood, Turret turret, Indexer indexer){
         this.shooter = shooter;
-        this.swerve = swerve;
+        this.drivetrain = drivetrain;
         this.hood = hood;
         this.turret = turret;
         this.indexer = indexer;
 
-        addRequirements(shooter, hood, indexer);
+        addRequirements(shooter, indexer);
     }
 
     @Override
     public void execute() {
-        if (turret.isOnTarget() && hood.isOnTarget() && shooter.isOnTarget()) {
+        if (turret.isOnTarget() & hood.isOnTarget() & shooter.isOnTarget()) {
             indexer.setSpindexerPower(Indexer.IndexerConstants.SPINDEXDER_POWER); //TEMP
             indexer.setTransferPower(Indexer.IndexerConstants.TRANSFER_POWER);
         }
 
-        Pose2d robotPose = swerve.getPose();
-
-        if(redAllianceZone.contains(robotPose.getTranslation())){
-            target = Swerve.FieldConstants.getTargetData(Swerve.FieldConstants.GOAL_POSITION);
-        }
-        else if (neutralZone.contains(robotPose.getTranslation())) {
-            target = Swerve.FieldConstants.getTargetData(Swerve.FieldConstants.DEPOT_POSITION);
-        }
-        else if(blueallianceZone.contains(robotPose.getTranslation())){
-            target = Swerve.FieldConstants.getTargetData(Swerve.FieldConstants.DEPOT_POSITION);
-        }
-
-        Distance distance = Meters.of(robotPose.getTranslation().getDistance(target));
-
+        Distance distance = Meters.of(drivetrain.getShooterTranslation().getDistance(drivetrain.getTargetPosition()));
         AngularVelocity shooterVelocity = ShooterConstants.VELOCITY_MAP.get(distance);
-        Angle hoodAngle = HoodConstants.HOOD_MAP.get(distance);
 
-        hood.setPosition(hoodAngle);
         shooter.setVelocity(shooterVelocity);
     }
 
     @Override
-    public void end(boolean interrupted) {
-        shooter.stop();
-        hood.stop();
-    }
+    public void end(boolean interrupted){}
 
     @Override
     public boolean isFinished() {
