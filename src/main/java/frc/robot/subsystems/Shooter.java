06b017fb70a1c1a391d99d8acde19a4014d2c223
Advaith <advaithgirish12@gmail.com>
2026-02-24 19:03:49 -0500

    changed the return run for runShootCommand to a new RunCommand

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -35,6 +35,7 @@ import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
@@ -265,7 +266,7 @@ public class Shooter extends SubsystemBase {
      * @return command for constantly running the shooter
      */
     public Command runShootCommand(Supplier<AngularVelocity> velocitySupplier) {
-        return run(() -> setVelocity(velocitySupplier.get()));
+        return new RunCommand(() -> setVelocity(velocitySupplier.get()));
     }
 
     /**
