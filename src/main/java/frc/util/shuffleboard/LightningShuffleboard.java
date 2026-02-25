438e15b0523cae477fd0a9ac6f2c0fde22297097
Karthik <krajendran275@gmail.com>
2026-02-24 19:34:09 -0500

    lightning shuffleboard fix

--- src/main/java/frc/util/shuffleboard/LightningShuffleboard.java
+++ src/main/java/frc/util/shuffleboard/LightningShuffleboard.java
@@ -463,14 +463,12 @@ public class LightningShuffleboard {
     }
 
     /**
-     * @deprecated use {@link #send(String, String, Sendable)} instead
      *
      * Set a {@link <a href="https://docs.wpilib.org/en/stable/docs/software/telemetry/robot-telemetry-with-sendable.html">Sendable</a>} object to NT through shuffleboard
      * @param tabName the tab this shuffleboard entry will be placed in
      * @param key the name of the shuffleboard entry
      * @param value the value of the shuffleboard entry
      */
-    @Deprecated
     public static void set(String tabName, String key, Sendable value) {
         ShuffleboardTab tab = Shuffleboard.getTab(tabName);
 
