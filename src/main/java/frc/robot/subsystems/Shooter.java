5c17cd7dee5fcf50729e833ed1c8927f6bcf1754
EduardoBarreto3006 <eduardorsbarreto@gmail.com>
2026-02-28 15:20:28 -0500

    [#391] Re-fix the bug

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -305,6 +305,6 @@ public class Shooter extends SubsystemBase {
      * @return the command for running the shooter at coast power
      */
     public Command coast() {
-        return startEnd(() -> setPower(ShooterConstants.COAST_DC), () -> {});
+        return run(() -> setPower(ShooterConstants.COAST_DC));
     }
 }
