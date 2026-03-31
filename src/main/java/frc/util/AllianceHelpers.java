df2156deb6b93d4739599aec4d6c8e2536040825
EduardoBarreto3006 <eduardorsbarreto@gmail.com>
2026-03-31 15:42:01 -0400

    [#498] Added the isHubActive() function and tested it in sim

--- src/main/java/frc/util/AllianceHelpers.java
+++ src/main/java/frc/util/AllianceHelpers.java
@@ -1,6 +1,10 @@
 package frc.util;
 
+import java.util.Optional;
+
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.util.shuffleboard.LightningShuffleboard;
 
 public class AllianceHelpers {
 
@@ -16,5 +20,59 @@ public class AllianceHelpers {
     */
     public static boolean isRedAlliance() {
         return !isBlueAlliance();
+    }
+
+    public boolean isHubActive() {
+        // Hub is always enabled in autonomous.
+        if (DriverStation.isAutonomousEnabled()) {
+            return true;
+        }
+
+        // At this point, if we're not teleop enabled, there is no hub.
+        if (!DriverStation.isTeleopEnabled()) {
+            return false;
+        }
+
+        // We're teleop enabled, compute.
+        double matchTime = DriverStation.getMatchTime();
+        String gameData = DriverStation.getGameSpecificMessage();
+        
+        // If we have no game data, we cannot compute, assume hub is active, as its likely early in teleop.
+        if (gameData.isEmpty()) {
+            return true;
+        }
+
+        boolean redInactiveFirst = false;
+        switch (gameData.charAt(0)) {
+            case 'R' -> redInactiveFirst = true;
+            case 'B' -> redInactiveFirst = false;
+            default -> {
+                // If we have invalid game data, assume hub is active.
+                return true;
+            }
+        }
+
+        // Shift one is active for blue if red won auto, or red if blue won auto.
+        boolean shift1Active = isBlueAlliance() ? redInactiveFirst : !redInactiveFirst;
+
+        if (matchTime > 130) {
+            // Transition shift, hub is active.
+            return true;
+        } else if (matchTime > 105) {
+            // Shift 1
+            return shift1Active;
+        } else if (matchTime > 80) {
+            // Shift 2
+            return !shift1Active;
+        } else if (matchTime > 55) {
+            // Shift 3
+            return shift1Active;
+        } else if (matchTime > 30) {
+            // Shift 4
+            return !shift1Active;
+        } else {
+            // End game, hub always active.
+            return true;
+        }
     }
 }
