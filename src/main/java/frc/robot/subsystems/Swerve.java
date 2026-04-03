568b8c10f505e545c41ef081fa26b6c14394cdb6
NotItAadit <aaditisntit@gmail.com>
2026-04-03 12:41:01 -0400

    revert claude changes

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -434,15 +434,26 @@ public class Swerve extends TunerSwerveDrivetrain implements Subsystem {
         ChassisSpeeds speeds = getCurrentRobotChassisSpeeds();
         double dt = time.in(Seconds);
 
+        Pose2d pose = getPose();
+
+        double sin = pose.getRotation().getSin();
+        double cos = pose.getRotation().getCos();
+
+        double rrXVel = (speeds.omegaRadiansPerSecond * Cannon.CannonConstants.SHOOTER_TRANSLATION.getX());
+        double rrYVel = (speeds.omegaRadiansPerSecond * Cannon.CannonConstants.SHOOTER_TRANSLATION.getY());
+
+        double frXVel = (rrXVel * cos) - (rrYVel * sin);
+        double frYVel = (rrXVel * sin) + (rrYVel * cos);
+
         double driveMultiplier = LightningShuffleboard.getDouble("Cannon", "OTF Multiplier", 1);
 
         Twist2d twist = new Twist2d(
-            speeds.vxMetersPerSecond * dt * driveMultiplier,
-            speeds.vyMetersPerSecond * dt * driveMultiplier,
-            speeds.omegaRadiansPerSecond * dt * driveMultiplier
+            (speeds.vxMetersPerSecond + frXVel) * dt * driveMultiplier,
+            (speeds.vyMetersPerSecond + frYVel) * dt * driveMultiplier,
+            0
         );
 
-        return getPose().exp(twist);
+        return pose.exp(twist);
     }
  
     public void configurePathplanner(){
