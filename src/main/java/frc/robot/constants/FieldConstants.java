f36ace64dddf9c3f6c90a7a8e0e0ebebd43cb084
Karthik <krajendran275@gmail.com>
2026-04-09 19:35:21 -0400

    [#558] passing changes and a lil auton stuffs

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -10,7 +10,7 @@ public class FieldConstants {
     public record Target(Translation2d blue, Translation2d red) {}
 
     public static final Target GOAL_POSITION = new Target(new Translation2d(4.625594, 4.034536), new Translation2d(11.915394, 4.034536));
-    public static final Target DEPOT_POSITION = new Target(new Translation2d(0.3937, 0.665988), new Translation2d(16.147288, 7.403338));
+    public static final Target DEPOT_POSITION = new Target(new Translation2d(3.735, 0.619), new Translation2d(12.644, 0.659));
 
     public static Translation2d getTargetData(Target target) {
         return AllianceHelpers.isBlueAlliance() ? target.blue() : target.red();
